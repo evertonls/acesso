03213f34d794479cc1a87e766c792fb06c45da0d
tinho <tinho@192.168.0.6>
2020-09-29 16:16:15 -0300

    Iniciando os Daos

---
+++ src/main/java/fortaleza/ce/gov/br/acesso/daos/VisitanteDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fortaleza.ce.gov.br.acesso.daos;
+
+import fortaleza.ce.gov.br.acesso.models.Visitante;
+
+/**
+ *
+ * @author tinho
+ */
+public interface VisitanteDao {
+    public void persist (Visitante visit);
+    public Visitante getVisitanteByCPF(String cpf);
+    public Visitante getVisitanteByPlaca(String placa);
+            
+    
+}
---
+++ src/main/java/fortaleza/ce/gov/br/acesso/daos/VisitanteDaoImpl.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fortaleza.ce.gov.br.acesso.daos;
+
+import fortaleza.ce.gov.br.acesso.models.Visitante;
+import java.io.Serializable;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author tinho
+ */
+public class VisitanteDaoImpl implements VisitanteDao, Serializable {
+    private static final long serialVersionUID = 1L;
+    private JdbcTemplate template;
+    private Logger logger;
+
+   
+    public VisitanteDaoImpl(DataSource ds, Logger logger) {
+        this.template = new JdbcTemplate(ds);
+        this.logger = logger;
+    }
+
+    @Override
+    public void persist(Visitante visit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Visitante getVisitanteByCPF(String cpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Visitante getVisitanteByPlaca(String placa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
